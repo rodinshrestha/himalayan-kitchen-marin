9da6118be9e957f13ca470686165f068c475e3b3
rodinshrestha <rodinshrestha@gmail.com>
2024-07-16 08:05:23 +0545

    fix: send mailer test

--- functions/sendEmail.ts
+++ functions/sendEmail.ts
@@ -1,4 +1,5 @@
 require("dotenv").config();
+const axios = require("axios");
 
 exports.handler = async (event, context) => {
   const { name, email, message } = JSON.parse(event.body);
@@ -18,27 +19,27 @@ exports.handler = async (event, context) => {
     text: message,
   };
 
-  fetch("https://api.mailersend.com/v1/email", {
-    method: "POST",
-    body: JSON.stringify(data),
-    headers: {
-      Authorization: `Bearer mlsn.eb98adaad5577ff86981f2ded4e07fcde0051060d852bd14d42836b5d6b3169b`,
-      "Content-Type": "application/json",
-    },
-  })
-    .then((res) => {
-      if (!res.ok) {
-        throw "";
+  try {
+    const response = await axios.post(
+      "https://api.mailersend.com/v1/email",
+      data,
+      {
+        headers: {
+          Authorization: `Bearer mlsn.eb98adaad5577ff86981f2ded4e07fcde0051060d852bd14d42836b5d6b3169b`,
+          "Content-Type": "application/json",
+        },
       }
-      return {
-        statusCode: 200,
-        body: JSON.stringify({ message: "Email sent successfully" }),
-      };
-    })
-    .catch(() => {
-      return {
-        statusCode: 500,
-        body: JSON.stringify({ error: "Failed to send email" }),
-      };
-    });
+    );
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify({ message: "Email sent successfully" }),
+    };
+  } catch (error) {
+    console.error("Error sending email:", error.message);
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ error: "Failed to send email" }),
+    };
+  }
 };
